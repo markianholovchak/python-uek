091cd920667a73cb3e7a74ee02a962c66a187493
markianholovchak <markiangolovchak@gmail.com>
2021-10-13 13:02:09 +0200

    Do class programs

---
+++ cw-2/15.py
@@ -0,0 +1,4 @@
+tempCelsius = float(input("Enter temperature in Celsius: "))
+tempFahrenheit = tempCelsius * 1.8 + 32
+tempKelvin = tempCelsius + 273.15
+print(f"Temp w Celsius: {tempCelsius}, Temp w Fahrenheit: {tempFahrenheit}, Temp w Kalvin: {tempKelvin}")
