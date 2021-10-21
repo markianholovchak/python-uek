0de290952b2e822500118f29af5962795857ab1a
markianholovchak <markiangolovchak@gmail.com>
2021-10-21 17:09:24 +0200

    Add program that prints a tree

--- cw-3/after-class/24.py
+++ cw-3/after-class/24.py
@@ -3,9 +3,7 @@ num = int(input("Enter number: "))
 mid = num // 2
 for i in range(num):
     if i <= mid:
-        for j in range(i + 1):
-            print("* ", end="")
+            print("* "*(i+1))
     else:
-        for z in range(num - i):
-            print("* ", end="")
-    print("")
+            print("* "*(num-i))
+    
