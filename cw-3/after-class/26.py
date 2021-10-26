d13c84373329a023e439587201558ad2b648b761
markianholovchak <markiangolovchak@gmail.com>
2021-10-26 20:43:49 +0200

    Minor fixed

--- cw-3/after-class/26.py
+++ cw-3/after-class/26.py
@@ -1,5 +1,5 @@
 PIN = "0805"
-attempts = 3
+attempts = 2
 loggedIn = False
 while not loggedIn and attempts > 0:
     userInput = input("Enter the PIN code: ")
@@ -11,4 +11,13 @@ while not loggedIn and attempts > 0:
         attempts -= 1
 if(attempts == 0):
     print("Sorry, your payment card has been blocked")
-    
+
+# userInput = input("Enter the PIN code: ")
+# while(userInput != PIN and attempts > 0):
+#     attempts -= 1
+#     print("Incorrect...")
+#     userInput = input("Enter the PIN code: ")
+# if(userInput == PIN):
+#     print("Successfuly authentificated")
+# else:
+#     print("Sorry, your payment card has been blocked")
--- cw-3/after-class/32.py
+++ cw-3/after-class/32.py
@@ -1,9 +1,4 @@
 for i in range(1, 8):
-    couponNum = i
-    for j in range(1, 8):
-        if(j == 1):
-            print(f" {i}", end="")
-        else:
-            couponNum += 7
-            print(f" {couponNum}", end="")
+    for j in range(i, 43+i, 7):
+        print(j, end=" ")
     print()
