8f93a4a02d06b72e0b0ee8bced43e9cae4f9ff39
markianholovchak <markiangolovchak@gmail.com>
2021-10-27 08:30:36 +0200

    Minor fix

--- cw-3/after-class/30.py
+++ cw-3/after-class/30.py
@@ -6,9 +6,9 @@ outputStr = "Prime numbers: "
 while(counter < numOfPrimes):
     """
     Look for prime numbers until enough
-     - Check if the checked number is divisible by some other integer between 1 and itself
+     - Check if the checked number is divisible by some other integer between 2 and itself
      - If it is, then it is not a prime number and we move to a number greater by 1
-     - If the number is not divisible by any other integer than 1 or itself then append the number
+     - If the number is not divisible by any other integer than 2 or itself then append the number
        to the output string and continue checking next number
     """
     checkStart = 2
@@ -21,9 +21,9 @@ while(counter < numOfPrimes):
         checkStart += 1
     if isPrime:
         # If we found a prime number then increase the counter and append the number to output string
-        counter +=1
+        counter += 1
         outputStr += f" {numberToCheck}"
+
     numberToCheck += 1
-        
+
 print(outputStr)
-        
