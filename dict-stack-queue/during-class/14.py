aafec1843155c0976d6fcba7dca25345205909e4
markianholovchak <markiangolovchak@gmail.com>
2021-11-24 19:44:17 +0100

    Minor fixes

--- dict-stack-queue/during-class/14.py
+++ dict-stack-queue/during-class/14.py
@@ -8,5 +8,8 @@ book = {
     "hasSequel": True
 }
 
+# with open("favourite2.json", "w") as f:
+#     json.dump(book,f, indent=2)
+
 with open("favourite.json", "w") as f:
     f.write(json.dumps([book], indent=2))
